08911798bd7637cb62a3ed1e5ea2a2d47ba8ec95
Robert Alvarado <treborlp@gmail.com>
2021-03-11 22:31:14 -0500

    Emitir desde el cliente

--- models/server.js
+++ models/server.js
@@ -41,9 +41,15 @@ class Server {
     sockets() {
         this.io.on('connection', socket => {
             console.log("Cliente conectado", socket.id);
+
             socket.on('disconnect', () => {
                 console.log('Cliente desconectado');
             })
+
+            socket.on('enviar-mensaje', (payload) => {
+                console.log(payload);
+            })
+
         });
     }
 
--- public/js/client-socket.js
+++ public/js/client-socket.js
@@ -1,6 +1,8 @@
 //Referencias del HTML
 const lblOnline = document.querySelector('#lblOnline');
 const lblOffline = document.querySelector('#lblOffline');
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar = document.querySelector('#btnEnviar');
 
 
 const socket = io();
@@ -14,4 +16,10 @@ socket.on('connect', () => { //Ayuda a asaber cuando se conecta al servidor
 socket.on('disconnect', () => { //Ayuda a asaber cuando se conecta al servidor
     lblOffline.style.display = ''
     lblOnline.style.display = 'none'
+})
+
+btnEnviar.addEventListener('click', () => {
+    const mensaje = txtMensaje.value;
+    //Enviar al servidor
+    socket.emit('enviar-mensaje', mensaje);
 })
